8128fb4fb75521f7b13b5f78dc1b4af08d1ca358
Mario Guzman <marioguzzzman@gmail.com>
2017-09-03 14:11:40 -0300

    arregle problemas de merge

--- php/registro.php
+++ php/registro.php
@@ -30,16 +30,18 @@
     <div class="row">
       <div class="col-xs-10 col-xs-offset-1 col-sm-6 col-sm-offset-3 col-lg-4 col-lg-offset-4">
         <header>
-          <h1>Registro</h1>
+          <h2>Registro</h2>
         </header>
       </div>
     </div>
 
     <form action="login.controller.php" method="POST" class="register-form">
 
+      <!--CAMPOS PRINCIPALES-->
+
       <div class="row">
         <div class="col-xs-10 col-xs-offset-1 col-sm-6 col-sm-offset-3 col-lg-4 col-lg-offset-4 form-group">
-          <label class="sr-only control-label" for="primer-nombre">Nombre</label>
+          <label class="sr-only control-label" for="primer-nombre">Nombre y Apellido</label>
           <input name="nombre" value="" class="form-control" type="text" placeholder="Nombre">
         </div>
       </div>
@@ -57,59 +59,75 @@
         <input name="password" value="" class="form-control" type="password" placeholder="Password">
       </div>
     </div>
+    <!-- /CAMPOS PRINCIPALES-->
+
+
+    <div class="row">
+      <!-- SEXO    /REVISAR FUNCIONAMIENTO-->
+      <div class="form-check form-check-inline col-xs-10 col-xs-offset-1 col-sm-6 col-sm-offset-3 col-lg-4 col-lg-offset-4 form-group">
+        <label class="form-check-label"> Sexo
+          <br>
+          <input class="form-check-input" type="radio" id="inlineCheckbox1" value="masculino">
+          Maculino
+          <input class="form-check-input" type="radio" id="inlineCheckbox2" value="femenino">
+          Femenino
+          <input class="form-check-input" type="radio" id="inlineCheckbox2" value="otro">
+          Otro
+        </label>
+      </div>
+      <!-- /SEXO-->
+    </div>
+
+    <div class="row">
+      <div class="form-check col-xs-10 col-xs-offset-1 col-sm-6 col-sm-offset-3 col-lg-4 col-lg-offset-4 form-group">
+        <hr>
+      </div>
+    </div>
 
       <div class="row">
+        <!-- ACTIVIDADES-->
         <div class="form-check col-xs-10 col-xs-offset-1 col-sm-6 col-sm-offset-3 col-lg-4 col-lg-offset-4 form-group">
           <label class="form-check-label">
-          ¿Qué tipo de actividades te interesan más? <br/>
-          <br/>
-            <input class="form-check-input" name="sitio[internet]" type="checkbox" value="internet">
+          ¿Qué actividades te interesan? <br/>
+            <input class="form-check-input" name="actividad[literatura]" type="checkbox" value="literatura">
               Literatura
-            <input class="form-check-input" name="sitio[amigos]" type="checkbox" value="amigos">
+            <input class="form-check-input" name="actividad[arte]" type="checkbox" value="arte">
               Arte
-            <input class="form-check-input" name="sitio[paloma]" type="checkbox" value="paloma">
+            <input class="form-check-input" name="actividad[cine]" type="checkbox" value="cine">
               Cine
-            <input class="form-check-input" name="sitio[paloma]" type="checkbox" value="paloma">
+            <input class="form-check-input" name="actividad[naraccion]" type="checkbox" value="naraccion">
               Narración
+              <input class="form-check-input" name="actividad[otra]" type="checkbox" value="otra">
+              Otras
           </label>
         </div>
+        <!--/ACTIVIDADES -->
       </div>
 
-        <div class="row">
-          <div class="form-check form-check-inline col-xs-10 col-xs-offset-1 col-sm-6 col-sm-offset-3 col-lg-4 col-lg-offset-4 form-group">
-            <label class="form-check-label"> Sexo
-              <br>
-              <input class="form-check-input" type="radio" id="inlineCheckbox1" value="option1">
-              Maculino
-              <input class="form-check-input" type="radio" id="inlineCheckbox2" value="option2">
-              Femenino
-              <input class="form-check-input" type="radio" id="inlineCheckbox2" value="option2">
-              Otro
-            </label>
-          </div>
-        </div>
-
-
-      <!-- ENCUESTA -->
 
       <div class="row">
-        div.col
+        <!-- ENCUESTA -->
+        <div class="col-xs-10 col-xs-offset-1 col-sm-6 col-sm-offset-3 col-lg-4 col-lg-offset-4 form-group">
         <select class="custom-select">
             <option selected>¿Cuántos libros lees por año?</option>
+            <option value="0">Ninguno</option>
             <option value="1">Uno a cinco</option>
             <option value="2">Cinco a diez</option>
             <option value="3">Más de diez</option>
         </select>
+        </div>
+        <!-- /ENCUESTA -->
       </div>
 
 
-      <!-- BOTON -->
       <form class="form-inline" action="" method="post">
+        <!-- BOTON -->
         <div class="row form-group">
           <div class="col-xs-10 col-xs-offset-1 col-sm-6 col-sm-offset-3 col-lg-4 col-lg-offset-4 form-group">
             <button class="btn btn-mg btn-block regbutton">Registrate</button>
           </div>
         </div>
+        <!-- BOTON -->
       </form>
 
 
