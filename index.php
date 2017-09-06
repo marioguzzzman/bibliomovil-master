1ba5a5609f23164eb2c3cb15fbda18d632a95d43
Mario Guzman <marioguzzzman@gmail.com>
2017-09-06 19:49:57 -0300

    css

--- index.php
+++ index.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Inicio</title>
 
-    <link rel="stylesheet" href="css/main-main.css">
     <link rel="stylesheet" href="css/navbar.css">
+    <link rel="stylesheet" href="css/main.css">
 
 
     <!-- Bootstrap -->
