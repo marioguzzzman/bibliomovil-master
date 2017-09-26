5a70bd9ad63c8aaf85f2650f09a9e499c5ea7043
Mario Guzman <marioguzzzman@gmail.com>
2017-09-25 23:51:00 -0300

    trivias

--- trivias/arte.php
+++ trivias/arte.php
@@ -30,6 +30,7 @@ $calificación = ['0','0','10',];
 
 		<link rel="stylesheet" href="trivias-main-css.css">
 
+
 		<!-- Bootstrap -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
