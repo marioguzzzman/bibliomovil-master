e99a276f43cef0511f02ecf326bde1d32ad7616a
Mario Guzman <marioguzzzman@gmail.com>
2017-10-18 18:59:01 -0300

    cambios menores en db

--- db/pdo.php
+++ db/pdo.php
@@ -10,3 +10,24 @@ try {
 } catch (Exception $e) {
   echo "La conexion a la base de datos falló: " . $e->getMessage();
 }
+
+
+//
+//
+// $host = 'localhost';
+// $base = 'usuarios.json';
+// $usuario = 'root';
+// $password = 'root';
+// $dsn = "mysql:host=$host;dbname=$base;charset=utf8mb4;";
+//
+// try { // solo puede capturar los errores que yo le diga
+//
+//   //CONEXIÓN A LA BASE
+//   // $db = new PDO('mysql:host=MI_HOST;dbname=MI_BASE;charset=utf8mb4', 'MI_USUARIO', 'MI_CLAVE');
+//   $db = new PDO($dsn,$usuario, $password); // sin espacios
+//     // echo 'estoy conectado';
+// } catch (PDOException $e) { //si ocurre el error entra al catch pero sino conincide sigue derecho
+//
+// echo "La conexion a la base de datos falló: " . $e->getMessage();
+//   // echo $e->getMessage();
+// }
